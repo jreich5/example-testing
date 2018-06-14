9796e987381d5bd22c607556a41f0b4c960dd577
Glitch (ten-functions) <none>
2018-06-14 04:58:32 +0000

    🎈🏪 Checkpoint ./tests.js:282805/559

--- tests.js
+++ tests.js
@@ -15,17 +15,25 @@
  */
  
 
-const randomBoolean = Boolean(Math.round(Math.random()));
-const randomNumber = Math.ceil(Math.random() * 100);
+const randomBoolean = Boolean(Math.round(Math.random()))
+const randomNumber = Math.ceil(Math.random() * 100)
 
 describe('isTrue', () => {
   it('should be a defined function', () => {
-    expect().toBe(true);
-  });
-  it('should return true when passed the boolean true as the input', () => {
+    expect(typeof isTrue).toBe('function')
+  })
+  it('should return a boolean value', () => {
+    expect(isTrue(typeof isTrue(randomBoolean)).toBe('boolean'))
+    expect(isTrue(typeof isTrue(false)).toBe('boolean'))
+    expect(isTrue(typeof isTrue(randomNumber)).toBe('boolean'))
+  })
+  it('should return true when passed the boolean true', () => {
+    expect(isTrue(true)).toBe(true);
+  })
+  it('should return false when passed the boolean false as the input', () => {
     
-  });
+  })
   it('should return false when passed the boolean false as the input', () => {
   
-  });
-});
+  })
+})
