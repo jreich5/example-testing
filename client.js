52c23352100c8098d7681e6544ee634cfcb814d2
Glitch (ten-functions) <none>
2018-06-16 22:02:00 +0000

    📙🍭 Checkpoint ./readme.html:282805/47 ./client.js:282805/540

--- client.js
+++ client.js
@@ -1,16 +1,15 @@
 "use strict";
 
 // Add functionality for updating count of number of remixes
-const projectURL
-document.querySelector("#remix").addEventListener("click", function(e) {
-  // increment the remix counter
-  // create a new remixer object with the following properties and push it onto a remixers array
-  // userAgentString 
-  // ipAddress
-});
- 
-const projectName = location.host.substring(0, location.host.indexOf("."));
+// document.querySelector("#remix").addEventListener("click", function(e) {
+//   // increment the remix counter
+//   // create a new remixer object with the following properties and push it onto a remixers array
+//   // userAgentString 
+//   // ipAddress
+// });
+
+let projectName = location.host.substring(0, location.host.indexOf("."));
       
-const editSolutionsUrl = "https://glitch.com/edit/#!/"+ projectName + "?path=solutions.js"; 
+let editSolutionsUrl = "https://glitch.com/edit/#!/"+ projectName + "?path=solutions.js"; 
       
 document.getElementById("solutions-edit-link").href = editSolutionsUrl;
