cb6f023d53e6ddeb2795b4beefe5b34f389f289b
Glitch (ten-functions) <none>
2018-06-16 17:24:31 +0000

    🔔⛈ Checkpoint ./index.html:282805/945 ./style.css:282805/55 ./client.js:282805/579

---
+++ client.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Add functionality for updating offsite co
+const projectURL
+document.querySelector("#remix").addEventListener("click", function(e) {
+  // increment the remix counter
+  // create a new remixer object with the following properties and push it onto a remixers array
+  // userAgentString 
+  // ipAddress
+});
