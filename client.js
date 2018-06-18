45841b594b003a8c901aa1f785ba2c9b053f7cc7
Glitch (ten-functions) <none>
2018-06-18 14:49:42 +0000

    🎿⛪️ Checkpoint ./client.js:282805/103

--- client.js
+++ client.js
@@ -13,3 +13,4 @@ let projectName = location.host.substring(0, location.host.indexOf("."));
 let editSolutionsUrl = "https://glitch.com/edit/#!/"+ projectName + "?path=solutions.js";       
 document.getElementById("solutions-edit-link").href = editSolutionsUrl;
 
+// Add tracker to count number of remixes/forks of the project
