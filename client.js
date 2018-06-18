0dcc7484ea3088ce93001f4faebabc41d18f363f
Glitch (ten-functions) <none>
2018-06-18 14:39:39 +0000

    🌄🙏 Checkpoint ./client.js:282805/51

--- client.js
+++ client.js
@@ -8,8 +8,8 @@
 //   // ipAddress
 // });
 
+// projectName and projectEditLink
 let projectName = location.host.substring(0, location.host.indexOf("."));
-      
-let editSolutionsUrl = "https://glitch.com/edit/#!/"+ projectName + "?path=solutions.js"; 
-      
+let editSolutionsUrl = "https://glitch.com/edit/#!/"+ projectName + "?path=solutions.js";       
 document.getElementById("solutions-edit-link").href = editSolutionsUrl;
+
