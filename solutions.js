383d3197735b623a84e31e0ec67d675a648a33cd
Justin Reich <justinreich.dev@gmail.com>
2020-12-17 13:21:21 -0600

    refactor: Add additional example notes

--- solutions.js
+++ solutions.js
@@ -1,45 +1,72 @@
 "use strict";
 
-// consider the following function that is meant to test other functions...
-function assert(callback, specDescription, input, expectedOutput) {
 
-    // call the passed function and store in a result
-    var result = callback(input);
+// ==================== Simple Example Test Function
 
-    // build the string for the possible fail message
-    var failMessage = "%c"
-        + specDescription
-        + " ===== FAILED: \n\tan input of "
-        + input
-        + " did not return "
-        + expectedOutput
-        + "\n\t"
-        + result
-        + " was returned instead";
+// function testAFunction(callback, input, expectedOutput) {
+//     return callback(input) === expectedOutput;
+// }
+//
+// function returnInputAsOutput(input) {
+//     return 'input';
+// }
+//
+// console.log(testAFunction(returnInputAsOutput, 'bob', 'bob'));
+// console.log(testAFunction(returnInputAsOutput, 2, 2));
+// console.log(testAFunction(returnInputAsOutput, true, true));
 
-    // build the string for the pass message
-    var passMessage =" %c" + specDescription + " ===== PASSED";
 
-    // compare the expected result with the actual result and log the outcome
-    if (result !== expectedOutput) {
-        console.log(failMessage, "color : red");
-    } else {
-        console.log(passMessage, "color: green");
-    }
 
-}
+
+
+// ==================== Fancy Example Test Function
+
+//
+// // consider the following function that is meant to test other functions...
+// function assert(callback, specDescription, input, expectedOutput) {
+//
+//     // call the passed function and store in a result
+//     var result = callback(input);
+//
+//     // build the string for the possible fail message
+//     var failMessage = "%cFAILED ===== "
+//         + specDescription
+//         + ": \n\t- an input of "
+//         + input
+//         + " did not return "
+//         + expectedOutput
+//         + "\n\t- "
+//         + result
+//         + " was returned instead";
+//
+//     // build the string for the pass message
+//     var passMessage ="%cPASSED ===== " + specDescription;
+//
+//     // compare the expected result with the actual result and log the outcome
+//     if (result !== expectedOutput) {
+//         console.log(failMessage, "color : red");
+//     } else {
+//         console.log(passMessage, "color: green");
+//     }
+//
+// }
+
+
 
 // here is a very simple pure function to test
-function increaseNumByTwo(num) {
-    return parseFloat(num) + 2;
-}
+// function increaseNumByTwo(num) {
+//     return (!isNaN(num)) ? Number(num) + 2 : true;
+// }
+//
+// // running the test...
+// assert(increaseNumByTwo, "it should increase a numeric input by two", 2, 4);
+//
+// // lets run a few more tests...
+// assert(increaseNumByTwo, "it should return false if any input is not numeric", "bob", false);
+// assert(increaseNumByTwo, "it should return 5.5 if passed 3.5", 3.5, 5.5);
+// assert(increaseNumByTwo, "it should return 10 if passed the string '8'", '8', 10);
 
 
-// running the test...
-assert(increaseNumByTwo, "it should increase a numeric input by two", 2, 4);
+// ==================== Jasmine Tests
 
-// lets run a few more tests...
-assert(increaseNumByTwo, "it should return false if any input is not numeric", "bob", false);
-assert(increaseNumByTwo, "it should return 5.5 if passed 3.5", 3.5, 5.5);
-assert(increaseNumByTwo, "it should return 10 if passed the string '8'", '8', 10);
-
+// TODO: Create a function, concat, that will take in two inputs and return the inputs concatenated together
--- tests.js
+++ tests.js
@@ -5,23 +5,23 @@
 const randomBoolean = Boolean(Math.round(Math.random()));
 const randomNumber = Math.ceil(Math.random() * 100);
 
-describe('concat', () => {
-  it('should be a defined function', () => {
+describe('concat', function() {
+  it('should be a defined function', function() {
     expect(typeof concat).toBe('function');
   })
-  it('should return a string when passed to strings', () => {
+  it('should return a string when passed to strings', function() {
     expect(typeof concat("hello", "world")).toBe('string')
   })
-  it('should return "helloworld" when passed "hello" and "world" as the two inputs.', () => {
+  it('should return "helloworld" when passed "hello" and "world" as the two inputs.', function() {
     expect(concat("hello", "world")).toBe("helloworld");
   })
-  it('should return "Codeup" when passed "Code" and "up" as the two inputs', () => {
+  it('should return "Codeup" when passed "Code" and "up" as the two inputs', function() {
     expect(concat("Code", "up")).toBe("Codeup")
   })
-  it('should return "42" when passed the number 4 and 2, treating each input as a string', () => {
+  it('should return "42" when passed the number 4 and 2, treating each input as a string', function() {
     expect(concat(4, 2)).toBe("42")
   })
-  it('should return "truetrue" when passed true and true', () => {
+  it('should return "truetrue" when passed true and true', function() {
     expect(concat(true, true)).toBe("truetrue")
   })
 }) 
